afa0eb74489557ea3acf8ed51966ada8ac3ef8db
Ahmad Bilal Farooqi <ahmadbilal20152016@gmail.com>
2024-09-17 10:02:34 +0500

    Add initial code files and configurations

---
+++ ch2/test.py
@@ -0,0 +1,24 @@
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_openai import OpenAI, ChatOpenAI
+from decouple import config
+SECRET_KEY = config('OPENAI_API_KEY')
+# print(SECRET_KEY)
+
+# Example 1 LLM's
+
+# llm = OpenAI(api_key=SECRET_KEY)
+# response = llm.invoke('who is the president of the United States?', )
+# print(response)
+
+# chatmodel
+# example 1
+# chat = ChatOpenAI(api_key=SECRET_KEY)
+# respnse = chat.invoke('who is the president of the United States?')
+# print(respnse.content)
+
+# example 2
+chat = ChatOpenAI(api_key=SECRET_KEY)
+messages = [HumanMessage('Who is the prime minister of India?'),
+            SystemMessage('You are a standup comedian.')]
+respnse = chat.invoke()
+print(respnse.content)
